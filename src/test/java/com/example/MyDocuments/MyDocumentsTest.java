a104b67d9918f471b47b2a7abfd34404586dc211
earl-stephens <stephensearl@hotmail.com>
2023-05-01 22:30:40 -0600

    Rough draft of test for model

---
+++ src/test/java/com/example/MyDocuments/MyDocumentsTest.java
@@ -0,0 +1,32 @@
+package com.example.MyDocuments;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
+import com.example.MyDocuments.service.SearchEngine;
+
+public class MyDocumentsTest {
+
+	SearchEngine engine = new StaticSearchEngine();
+	
+	@Test
+	void testFindByName() {
+		var documents = engine.findByType(PDF);
+		
+		assertNotNull(documents);
+		assertEquals(documents.size(), 1);
+		assertEquals(PDF.name, documents.get(0).getType().name);
+		assertEquals(PDF.desc, documents.get(0).getType().desc);
+		assertEquals(PDF.extension, documents.get(0).getType().extension);
+	}
+	
+	@Test
+	void testListAll() {
+		var documents = engine.listAll();
+		
+		assertNotNull(documents);
+		assertEquals(documents.size(), 4);
+	}
+}
