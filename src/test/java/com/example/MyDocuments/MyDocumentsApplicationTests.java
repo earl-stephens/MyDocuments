f3bdc21dd7b2deac07f2adff98f3546d1dee46e5
earl-stephens <stephensearl@hotmail.com>
2023-05-01 21:59:52 -0600

    Initial commit

---
+++ src/test/java/com/example/MyDocuments/MyDocumentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyDocuments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDocumentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
